2e1405e8781d7461e2dc3c463ed6504aca6ee070
vinayskywalker <vinaykumar.dakshana17@gmail.com>
2021-12-12 23:03:52 +0530

    update

--- myauthentication/urls.py
+++ myauthentication/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
-from django.views.static import serve
-from django.conf.urls import url
-from .import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myauth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
