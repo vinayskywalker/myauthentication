261211abc056ce4cc7235ac236d1ad67b8ede7f8
vinayskywalker <vinaykumar.dakshana17@gmail.com>
2021-12-13 00:13:19 +0530

    add

--- myauth/views.py
+++ myauth/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 
-# Create your views here.
 
-
-
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 
 def connect_google():
 	print("connecting")
+	flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file('client_secret.json',scopes=SCOPES)
+	flow.redirect_uri = 'https://myauth-django.herokuapp.com/oauth2callback'
+	authorization_url, state = flow.authorization_url(access_type='offline',include_granted_scopes='true')
+
+
 
 
 def login(request):
+	
 	if request.method == 'POST':
 		print(request)
+
 	return render(request,'login.html')
 
 
