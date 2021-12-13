ce9cfc2040fe5e188c88d2565885409016fc5d17
vinayskywalker <vinaykumar.dakshana17@gmail.com>
2021-12-13 10:13:19 +0530

    add

--- myauth/views.py
+++ myauth/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 from django.http import HttpResponseRedirect
+from googleapiclient.discovery import build
 
 
 
